8bf8be80b9d9b0387462711613178c794b207c7e
majklost <majkl.mrkos@seznam.cz>
2025-02-10 12:12:32 +0100

    initial structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='rrt_rl_2D',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'gymnasium',
+        'numpy',
+        'stable-baselines3',
+        'pymunk',
+        'pygame',
+        'tensorboard',
+    ],
+    author="Michal Mrkos",
+    author_email="mrkosmic@fel.cvut.cz",
+    description="2D RRT with Reinforcement Learning",
+    url="https://github.com/majklost/RRT-RL-2D",
+    python_requires='>=3.10',
+)
