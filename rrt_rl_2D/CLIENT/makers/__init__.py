33a83a67c2c8e084653091798d15a1aea62fab6f
majklost <majkl.mrkos@seznam.cz>
2025-03-02 14:59:05 +0100

    one and two controllable

--- rrt_rl_2D/CLIENT/makers/__init__.py
+++ rrt_rl_2D/CLIENT/makers/__init__.py
@@ -1,7 +1,7 @@
 # from .makers import BlendMaker, CableRadiusMaker, StandardCableMaker, \
 #     DebugMaker, RectMaker
 
-from .makers import CableRadiusMaker, DebugMaker, BlendMaker, StandardCableMaker, RectMaker, BlendStrengthMaker, CableInnerAnglesMaker
+from .makers import CableRadiusMaker, DebugMaker, BlendMaker, StandardCableMaker, RectMaker, BlendStrengthMaker, CableInnerAnglesMaker, CableBigTestMaker
 
 __all__ = ['CableRadiusMaker', 'DebugMaker',
-           'BlendMaker', 'StandardCableMaker', 'RectMaker', "BlendStrengthMaker", "CableInnerAnglesMaker"]
+           'BlendMaker', 'StandardCableMaker', 'RectMaker', "BlendStrengthMaker", "CableInnerAnglesMaker", "CableBigTestMaker"]
